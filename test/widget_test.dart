fa46c1763b0533cf497ec54f50b348a24efab0b3
SoSuddenly <122160671+SoSuddenly@users.noreply.github.com>
2023-09-12 16:22:24 +0300

    try some test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,26 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:test_color_generator/main.dart';
+import 'package:test_color_generator/main_screen.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('Color change test', (WidgetTester tester) async {
+    //trigger a frame.
+    await tester.pumpWidget(const MainScreen());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    const int testsCounts = 16777216;
+    //number of taps
+    for (int i = 0; i < testsCounts; i++) {
+      await tester.tap(find.byType(ColoredBox));
+    }
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    // try to catch our random but if we cant so be it :)
+    const expectedBackgroundColor = Colors.black;
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Check if the background color has indeed changed.
+    expect(
+      tester.widget<ColoredBox>(find.byType(ColoredBox)).color,
+      expectedBackgroundColor,
+    );
   });
 }
